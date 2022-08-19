9e6ae071f67c5052e4f3abe9fe56551dead9d9f2
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-08-19 17:07:04 -0300

    Primeiras funcionalidades javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
-function insereValores() {
+
+window.onload = () => {
+    let display = document.getElementById('display');
+    const CLEAR = document.getElementById('clear');
+    CLEAR.addEventListener('click', limpaDisplay);
     
+    function limpaDisplay() {
+        display.innerHTML = 0;
+    }
+}
+
+const insertNum = num => display.textContent += num;
+
+const resultado = () => {
+    //(display.textContent) ? display.textContent : false;
 }
