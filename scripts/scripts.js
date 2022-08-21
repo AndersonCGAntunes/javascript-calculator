181e29517732602d16bb87e70b9ea8b536a2bbd7
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-08-21 15:05:21 -0300

    Usando novos recursos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,13 @@
 
-window.onload = () => {
-    let display = document.getElementById('display');
-    const CLEAR = document.getElementById('clear');
-    CLEAR.addEventListener('click', limpaDisplay);
-    
-    function limpaDisplay() {
-        display.innerHTML = 0;
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
-}
-
-const insertNum = num => display.textContent += num;
-
-const resultado = () => {
-    //(display.textContent) ? display.textContent : false;
 }
